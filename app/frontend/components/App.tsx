3d314937a12d6eb0d25ee3168fe3df068f89da13
Rick Mark <rickmark@outlook.com>
2026-06-01 17:56:46 -0700

    Base tailwind / theme

--- app/frontend/components/App.tsx
+++ app/frontend/components/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 export const App: React.FC = ({}) => {
-    return (
-        <div>
-            Hello
-        </div>);
+    return <div className="flex min-h-screen items-center justify-center bg-slate-900">
+            <h1 className="text-4xl font-bold text-sky-400 drop-shadow-md">
+                React + Vite + Tailwind Setup Complete!
+            </h1>
+        </div>;
 }
